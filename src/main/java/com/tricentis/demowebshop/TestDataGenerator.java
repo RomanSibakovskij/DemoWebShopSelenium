3f7fb5b7448cc52e72acbfbd749d757e1d2ba842
RomanSibakovskij <j4023958@gmail.com>
2024-11-29 07:49:08 +0200

    Add empty TestDataGenerator sheet

---
+++ src/main/java/com/tricentis/demowebshop/TestDataGenerator.java
@@ -0,0 +1,14 @@
+package com.tricentis.demowebshop;
+
+import org.openqa.selenium.*;
+
+import java.security.SecureRandom;
+import java.util.*;
+
+public class TestDataGenerator extends BasePage{
+
+
+    public TestDataGenerator(WebDriver driver) {super(driver);}
+
+    
+}
