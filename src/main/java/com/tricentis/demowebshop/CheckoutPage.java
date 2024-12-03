098d48b27e88c075aa43f5331d56b37d94837eeb
RomanSibakovskij <j4023958@gmail.com>
2024-12-03 15:57:57 +0200

    Add empty CheckoutPage sheet

---
+++ src/main/java/com/tricentis/demowebshop/CheckoutPage.java
@@ -0,0 +1,11 @@
+package com.tricentis.demowebshop;
+
+import org.openqa.selenium.*;
+
+public class CheckoutPage extends BasePage{
+
+
+    public CheckoutPage(WebDriver driver) {super(driver);}
+
+
+}
