d3ff9a65df4c80067d0391f88eb232dc12728362
RomanSibakovskij <j4023958@gmail.com>
2024-12-03 07:23:17 +0200

    Add empty ShoppingCartPage sheet

---
+++ src/main/java/com/tricentis/demowebshop/ShoppingCartPage.java
@@ -0,0 +1,9 @@
+package com.tricentis.demowebshop;
+
+import org.openqa.selenium.*;
+
+public class ShoppingCartPage extends BasePage{
+
+    public ShoppingCartPage(WebDriver driver) {super(driver);}
+
+}
