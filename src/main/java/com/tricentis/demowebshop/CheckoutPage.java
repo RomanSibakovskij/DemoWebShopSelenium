4741a68bf76fc94e1849cf152bee878fc79fd731
RomanSibakovskij <j4023958@gmail.com>
2024-12-03 16:16:26 +0200

    Add some web elements for CheckoutPage

--- src/main/java/com/tricentis/demowebshop/CheckoutPage.java
+++ src/main/java/com/tricentis/demowebshop/CheckoutPage.java
@@ -1,9 +1,42 @@
 package com.tricentis.demowebshop;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
 
 public class CheckoutPage extends BasePage{
 
+    //checkout page web elements
+    @FindBy(xpath = "//div[@class='page-title']")
+    private WebElement checkoutPageTitle;
+    //billing address section web elements
+    @FindBy(xpath = "//ol[@id='checkout-steps']/li[@id='opc-billing']/div[@class='step-title']")
+    private WebElement billingAddressSectionTitle;
+    @FindBy(xpath = "//ol[@id='checkout-steps']/li[@id='opc-billing']//div[@class='enter-address-body']//input[@id='BillingNewAddress_FirstName']")
+    private WebElement firstNameInputField;
+    @FindBy(xpath = "//ol[@id='checkout-steps']/li[@id='opc-billing']//div[@class='enter-address-body']//input[@id='BillingNewAddress_LastName']")
+    private WebElement lastNameInputField;
+    @FindBy(xpath = "//ol[@id='checkout-steps']/li[@id='opc-billing']//div[@class='enter-address-body']//input[@id='BillingNewAddress_Email']")
+    private WebElement emailAddressInputField;
+    @FindBy(xpath = "//ol[@id='checkout-steps']/li[@id='opc-billing']//div[@class='enter-address-body']//input[@id='BillingNewAddress_Company']")
+    private WebElement companyInputField;
+    @FindBy(xpath = "//ol[@id='checkout-steps']/li[@id='opc-billing']//div[@class='enter-address-body']//select[@id='BillingNewAddress_CountryId']")
+    private WebElement countryDropdownMenu;
+    @FindBy(xpath = "//ol[@id='checkout-steps']/li[@id='opc-billing']//div[@class='enter-address-body']//select[@id='BillingNewAddress_StateProvinceId']")
+    private WebElement stateDropdownMenu;
+    @FindBy(xpath = "//ol[@id='checkout-steps']/li[@id='opc-billing']//div[@class='enter-address-body']//input[@id='BillingNewAddress_City']")
+    private WebElement cityInputField;
+    @FindBy(xpath = "//ol[@id='checkout-steps']/li[@id='opc-billing']//div[@class='enter-address-body']//input[@id='BillingNewAddress_Address1']")
+    private WebElement address1InputField; //required input
+    @FindBy(xpath = "//ol[@id='checkout-steps']/li[@id='opc-billing']//div[@class='enter-address-body']//input[@id='BillingNewAddress_Address2']")
+    private WebElement address2InputField;
+    @FindBy(xpath = "//ol[@id='checkout-steps']/li[@id='opc-billing']//div[@class='enter-address-body']//input[@id='BillingNewAddress_ZipPostalCode']")
+    private WebElement postalCodeInputField;
+    @FindBy(xpath = "//ol[@id='checkout-steps']/li[@id='opc-billing']//div[@class='enter-address-body']//input[@id='BillingNewAddress_PhoneNumber']")
+    private WebElement phoneNumberInputField;
+    @FindBy(xpath = "//ol[@id='checkout-steps']/li[@id='opc-billing']//div[@class='enter-address-body']//input[@id='BillingNewAddress_FaxNumber']")
+    private WebElement faxNumberInputField;
+    @FindBy(xpath = "//ol[@id='checkout-steps']/li[@id='opc-billing']//input[@class='button-1 new-address-next-step-button']")
+    private WebElement billingAddressContinueButton;
 
     public CheckoutPage(WebDriver driver) {super(driver);}
 
