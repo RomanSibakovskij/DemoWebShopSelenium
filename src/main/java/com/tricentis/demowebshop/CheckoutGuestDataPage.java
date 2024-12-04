7e2669d53e37d74a65a7fb616ac1a5f642ca154b
RomanSibakovskij <j4023958@gmail.com>
2024-12-04 16:32:36 +0200

    Add empty CheckoutGuestDataPage utility class sheet

---
+++ src/main/java/com/tricentis/demowebshop/CheckoutGuestDataPage.java
@@ -0,0 +1,9 @@
+package com.tricentis.demowebshop;
+
+import org.openqa.selenium.*;
+
+public class CheckoutGuestDataPage extends BasePage{
+
+    public CheckoutGuestDataPage(WebDriver driver) {super(driver);}
+
+}
