bfcf4d48b93de27a37e76f631c6e0a2bd8570e23
RomanSibakovskij <j4023958@gmail.com>
2024-12-05 06:29:08 +0200

    Add web elements for CheckoutPaymentMethodPage

--- src/main/java/com/tricentis/demowebshop/CheckoutPaymentMethodPage.java
+++ src/main/java/com/tricentis/demowebshop/CheckoutPaymentMethodPage.java
@@ -1,8 +1,24 @@
 package com.tricentis.demowebshop;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
 
 public class CheckoutPaymentMethodPage extends BasePage{
+
+    //credit card section input elements
+    @FindBy(xpath = "//div[@class='section payment-info']//table//tr[1]/td[2]/select")
+    private WebElement creditCardTypeDropdownMenu;
+    @FindBy(xpath = "//div[@class='section payment-info']//table//tr[2]/td[2]/input")
+    private WebElement creditCardNameInputField;
+    @FindBy(xpath = "//div[@class='section payment-info']//table//tr[3]/td[2]/input")
+    private WebElement creditCardNumberInputField;
+    @FindBy(xpath = "//div[@class='section payment-info']//table//tr[4]/td[2]/select[@id='ExpireMonth']")
+    private WebElement creditCardMonthInputField;
+    @FindBy(xpath = "//div[@class='section payment-info']//table//tr[4]/td[2]/select[@id='ExpireYear']")
+    private WebElement creditCardYearInputField;
+    @FindBy(xpath = "//div[@class='section payment-info']//table//tr[5]/td[2]/input")
+    private WebElement creditCardCVCInputField;
+
 
     public CheckoutPaymentMethodPage(WebDriver driver) {super(driver);}
 
