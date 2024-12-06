6436291582faaa64270b06a58298b9b308779d15
RomanSibakovskij <j4023958@gmail.com>
2024-12-06 08:34:00 +0200

    Add empty OrderDetailsPage sheet

---
+++ src/main/java/com/tricentis/demowebshop/OrderDetailsPage.java
@@ -0,0 +1,8 @@
+package com.tricentis.demowebshop;
+
+import org.openqa.selenium.*;
+
+public class OrderDetailsPage extends BasePage{
+
+    public OrderDetailsPage(WebDriver driver) {super(driver);}
+}
